00477c156069a8bd990c81e062ec7f8d5838df00
brettpac <brettpac@users.noreply.github.com>
2026-03-04 01:23:44 -0800

    Remove GitHub context from documentation config
    Remove GitHub context settings from conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,13 +35,5 @@ html_css_files = ['custom.css']
 
 html_show_sourcelink = False
 
-html_context = {
-    "display_github": True,
-    "github_user": "robosoft-ai",
-    "github_repo": "smacc2_docs",
-    "github_version": "main",
-    "conf_py_path": "/docs/source/",
-}
-
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
