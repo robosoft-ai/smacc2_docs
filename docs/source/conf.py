339474992577364c31d2b8a64ca126ac2bbda5de
brettpac <brettpac@users.noreply.github.com>
2026-03-03 20:59:21 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,5 +33,9 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_css_files = ['custom.css']
 
+html_context = {
+    "readthedocs_flyout_placement": "bottom-left",
+}
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
